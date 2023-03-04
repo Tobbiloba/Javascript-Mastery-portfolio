e3a78f740adc2c9e9fd0de6620c4e3e309154d5c
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-04 10:06:43 +0100

    fixed minor bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
-function App() {
-  return <div>3D Portfolio</div>
+import { BrowserRouter } from 'react-router-dom'
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-n0-repeat bg-center">
+          <Navbar />
+          <Hero></Hero>
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
