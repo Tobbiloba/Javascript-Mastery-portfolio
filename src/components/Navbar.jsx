0a090044d96955dd7dff5d3ba6708f551e89a166
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-04 11:21:19 +0100

    added hero text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,21 +12,22 @@ const Navbar = () => {
   return (
     <nav
       className={`
-    ${styles.paddindX} w-full items-center py-5 fixed top-0 z-20 bg-primary
+    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
     `}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
-          className="flex items-ceenter gap-2"
+          className="flex items-center gap-2"
           onClick={() => {
             setActive('')
             window.scrollTo(0, 0)
           }}
         >
-          <img src={logo} alt={logo} className="w-16 h-16 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Salau <span className="sm:block hidden">| Tobiloba</span>
+          <img src={logo} alt={logo} className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Salau &nbsp;
+            <span className="sm:block hidden">| Tobiloba</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
