c9aa59922b07bbb35a45c7039fa1de8c91ec9dce
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-10 00:04:40 +0100

    added computer image

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,8 +8,29 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
     </mesh>
   )
 }
-
+const computerCanvas = () => {
+  return (
+    <Canvas
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      frameloop="demand"
+      shadows
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
 export default Computers
